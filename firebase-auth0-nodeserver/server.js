ff6565d4ff5789f36305c414381153222123f3ee
assasindev <topguru.uefa@gmail.com>
2018-08-29 21:20:58 +0800

    add server.js

---
+++ firebase-auth0-nodeserver/server.js
@@ -0,0 +1,17 @@
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// App
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+// Set port
+const port = process.env.PORT || '1337';
+app.set('port', port);
+
+// Routes
+require('./routes')(app);
