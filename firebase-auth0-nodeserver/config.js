dc751c95d731bbe6d3d4ff6dffbdc52b26aefeb3
assasindev <topguru.uefa@gmail.com>
2018-08-30 06:10:26 +0800

    update config and add dogs.json

--- firebase-auth0-nodeserver/config.js
+++ firebase-auth0-nodeserver/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     AUTH0_DOMAIN: 'ninjadev1030.auth0.com', // e.g., you.auth0.com
     AUTH0_API_AUDIENCE: 'http://localhost:1337/', // e.g., http://localhost:1337/
-    FIREBASE_KEY: './firebase/angular-firebase-auth0-dd3c6-firebase-adminsdk-1zkjv-7e8781ac3a.json', // e.g., your-project-firebase-adminsdk-xxxxx-xxxxxxxxxx.json
+    FIREBASE_KEY: './firebase/angular-firebase-auth0-dd3c6-firebase-adminsdk-1zkjv-c19e5cea7d.json', // e.g., your-project-firebase-adminsdk-xxxxx-xxxxxxxxxx.json
     FIREBASE_DB: 'https://angular-firebase-auth0-dd3c6.firebaseio.com' // e.g., https://your-project.firebaseio.com
 };
